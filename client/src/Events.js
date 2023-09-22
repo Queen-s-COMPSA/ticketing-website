528a36816788311c8bec4c43e0a9f798776120f1
Daniel Dousek <danieldousek@gmail.com>
2023-09-22 13:25:20 -0400

    Events hidden/visable on button click

--- client/src/Events.js
+++ client/src/Events.js
@@ -3,6 +3,14 @@ import bg_image from "./images/Events.png";
 import EventCard from "./EventCard";
 
 function Events() {
+  const [isVisible, setIsVisible] = useState(false);
+
+  function toggleVisibility() {
+    const contentWrapper = document.getElementById("contentWrapper");
+    contentWrapper.classList.toggle("scale-100");
+    contentWrapper.classList.toggle("opacity-100");
+  }
+
   const events = [
     {
       title: "EVENT NAME HERE",
@@ -30,9 +38,9 @@ function Events() {
   return (
     <div className=''>
       {/* Events */}
-      <section className='flex flex-col items-center'>
+      <section className=' flex flex-col items-center'>
         {/* Upcoming Events */}
-        <section className='w-custon_max_w  flex items-center mt-52 mb-8'>
+        <section className='w-custon_max_w flex items-center mt-52 mb-8'>
           <span className='w-full h-1  bg-custom_yellow '></span>
           <h6 className='text-4xl text-center mx-4 whitespace-nowrap font-medium'>UPCOMING EVENTS</h6>
           <span className='w-full h-1  bg-custom_yellow'></span>
@@ -45,7 +53,28 @@ function Events() {
         </section>
         {/* All Events */}
         <section className='w-custon_max_w  flex flex-col items-center '>
-          <button className=' bg-custom_dark_purple text-white mt-44 mb-40 px-10 py-3 rounded-lg'>View All</button>
+          <div
+            id='contentWrapper'
+            className={` transition-all duration-300 ease-in-out ${isVisible ? "mt-40 scale-100 opacity-100" : "mt-0 scale-0 opacity-0"}`}
+          >
+            <div className={`w-full flex items-center `}>
+              <div className='flex-1 h-1 bg-custom_yellow'></div>
+              <h6 className='text-4xl text-center mx-4 whitespace-nowrap font-medium'>All Events</h6>
+              <div className='flex-grow h-1 bg-custom_yellow'></div>
+            </div>
+            <section id='contentContainer' style={{ height: isVisible ? "auto" : "0", overflow: "hidden" }}>
+              {events.map((event, index) => (
+                <EventCard key={index} {...event} />
+              ))}
+            </section>
+          </div>
+          <button
+            id='toggleButton'
+            className='bg-custom_dark_purple text-white mt-14 mb-40 px-10 py-3 rounded-lg'
+            onClick={() => setIsVisible(!isVisible)}
+          >
+            {isVisible ? "View Less" : "View All"}
+          </button>
         </section>
       </section>
     </div>
