e5023c760c3267d274f2f173bf114abc3f9d68f1
Daniel Dousek <danieldousek@gmail.com>
2023-10-15 01:51:46 -0400

    Mobile Friendly Events

--- client/src/Homepage.js
+++ client/src/Homepage.js
@@ -1,42 +1,21 @@
 import React, { useEffect, useState } from "react";
-import logo from "./images/logo.png";
-import cart from "./images/cart.svg";
-import cart_white from "./images/cart_white.svg";
 import polygon from "./images/Polygon.svg"; // This is the traingle shape at the bottom
 
-function App() {
+function HomePage() {
   return (
     <div className=''>
-      {/* Header */}
-      <header className='bg-custom_black p-3 flex justify-between items-center'>
-        <div className='flex items-center ml-4'>
-          <img src={logo} alt='Logo' className='h-10 w-10' />
-        </div>
-
-        <div className='flex space-x-4 text-white text-lg font-bold'>
-          <button className='py-2 px-4 hover:underline underline-offset-4 transition-all duration-300 ease-in-out'>EVENTS</button>
-          <button className='py-2 px-4 hover:underline underline-offset-4 transition-all duration-300 ease-in-out'>CONTACT</button>
-          <button className='py-3 px-8 text-custom_yellow rounded-lg border-2 hover:bg-custom_yellow hover:text-white border-custom_yellow flex items-center group'>
-            <div className='mr-2 group-hover:hidden '>
-              <img src={cart} alt='Cart Icon' />
-            </div>
-            <div className='mr-2 hidden group-hover:inline '>
-              <img src={cart_white} alt='Cart Icon' />
-            </div>
-            Tickets
-          </button>
-        </div>
-      </header>
-
       {/* Home Page */}
-      <section className='bg-custom-bg bg-hero-patternbg-cover bg-center h-screen max-h-custom_bg_h flex justify-center items-center '>
-        <div className='w-custon_max_w mt-96 m-10 '>
-          <h6 className='text-5xl font-bold text-custom_yellow '>COMPSA EVENTS</h6>
-          <p className='text-2xl pt-6 text-white'>
+      <section className='bg-custom-bg bg-cover bg-no-repeat bg-center h-screen flex justify-center items-center relative'>
+        {/* HomePage Content */}
+        <div className='absolute top-1/2 left-1/2 transform -translate-y-1/2  -translate-x-1/2 text-center px-5 sm:px-10'>
+          <h6 className='text-3xl sm:text-4xl md:text-5xl lg:text-6xl font-bold text-custom_yellow '>COMPSA EVENTS</h6>
+          <p className='text-xl sm:text-1xl md:text-2xl lg:text-3xl pt-4 sm:pt-5 md:pt-6 lg:pt-8 text-white'>
             Description about ticket purchasing or something? Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce quis dui sollicitudin,
             sagittis mauris id, venenatis leo.
           </p>
         </div>
+
+        {/* Info Content */}
       </section>
 
       {/* The triangle*/}
@@ -47,4 +26,4 @@ function App() {
   );
 }
 
-export default App;
+export default HomePage;
--- client/src/Info.js
+++ client/src/Info.js
@@ -6,40 +6,40 @@ import cart_white from "./images/cart_white.svg";
 function Info() {
   return (
     <div className='flex items-center justify-center'>
-      <section className='  w-custon_max_w mt-36 mb-36'>
+      <section className=' w-custon_max_w mt-36 mb-36 text-center sm:text-left'>
         {/* Location */}
-        <section className='min-h-custom_info_h'>
-          <section className=' flex items-center mb-2  '>
-            <img src={location} />
-            <h6 className='text-4xl pl-4 text-center pr-5 whitespace-nowrap font-medium'>Location & Time</h6>
-            <span className='w-full h-1  bg-custom_yellow'></span>
+        <section className='min-h-custom_info_h p-10 lg:p-0 flex flex-col items-center sm:items-start '>
+          <section className='sm:w-full flex items-center mb-2 '>
+            <img src={location} alt='Location Icon' />
+            <h6 className='text-4xl pl-4 text-center pr-0 sm:pr-5 whitespace-nowrap font-medium'>Location & Time</h6>
+            <span className='block w-0 sm:w-full h-1 bg-custom_yellow'></span>
           </section>
           <h1 className='font-bold mb-1 '>September 30th 9:00PM - 10:00PM EST</h1>
-          <p className=' w-4/5'>Goodwin Hall, Queen’s University</p>
+          <p className='w-4/5'>Goodwin Hall, Queen’s University</p>
         </section>
 
         {/* info */}
-        <section className=' min-h-custom_info_h'>
+        <section className=' min-h-custom_info_h p-10 lg:p-0 '>
           <section className=' flex items-center  '>
-            <h6 className='text-4xl text-center pr-5 whitespace-nowrap font-medium'>About this event</h6>
-            <span className='w-full h-1  bg-custom_yellow'></span>
+            <h6 className='text-4xl pr-5 m-auto whitespace-nowrap font-medium'>About this event</h6>
+            <span className='sm:w-full w-auto h-1 bg-custom_yellow'></span>
           </section>
-          <p className=' w-4/5'>
+          <p className=' sm:w-4/5 m-auto sm:m-0'>
             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce quis dui sollicitudin, sagittis mauris id, venenatis leo. Lorem ipsum dolor
             sit amet, consectetur adipiscing elit. Fusce quis dui sollicitudin, sagittis mauris id, venenatis leo.
           </p>
         </section>
 
         {/* Ticket */}
-        <section className=''>
-          <section className=' flex items-center  '>
+        <section className='p-10 lg:p-0 flex flex-col items-center sm:items-start'>
+          <section className='w-full sm:flex items-center  '>
             <h6 className='text-4xl text-center pr-5 whitespace-nowrap font-medium'>Tickets</h6>
-            <span className='w-full h-1  bg-custom_yellow'></span>
+            <span className='sm:w-full h-1 invisible sm:visible w-0  bg-custom_yellow'></span>
           </section>
-          <div className='flex font-bold'>
-            Ticket Fare: <div className='px-2 font-normal'> $5.00</div>
+          <div className='flex font-bold text-center'>
+            Ticket Fare: <div className='px-2 font-normal '> $5.00</div>
           </div>
-          <button className='mt-5 py-3 px-6 bg-custom_dark_purple text-white rounded-lg border-2 hover:bg-white hover:text-custom_purple border-custom_dark_purple flex items-center group'>
+          <button className='mt-5 py-3 px-6 bg-custom_dark_purple border-2 text-white rounded-lg  hover:bg-white hover:text-custom_purple border-custom_dark_purple flex items-center group'>
             <div className='mr-2 group-hover:hidden '>
               <img src={cart_white} alt='Cart Icon' />
             </div>
